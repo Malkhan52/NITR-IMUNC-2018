9e4b371f05d2bb1ad3e00c2830efcc3042ed0f51
Malkhan Singh <malkhansinghrathaur@gmail.com>
2017-12-29 03:37:51 +0530

    Update committees pages

--- mailer.php
+++ mailer.php
@@ -22,7 +22,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "malkhansinghrathaur@gmail.com";
+        $recipient = "secgen.nitrimun@gmail.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
